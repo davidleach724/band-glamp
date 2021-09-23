881e7603b19bbb8838d1ed5f2192281e42e46b2d
David Leach <davidleach724@gmail.com>
2021-09-23 17:26:24 -0600

    Add styling to single gigs

--- src/components/SmallDates/SmallDates.js
+++ src/components/SmallDates/SmallDates.js
@@ -3,7 +3,10 @@ import './SmallDates.css'
 const SmallDates = ({ show }) => {
   return (
     <section>
+      <p>{show.date}</p>
       <p>{show.venue}</p>
+      <p>{show.city}</p>
+      <p>{show.state}</p>
     </section>
   )
 }
