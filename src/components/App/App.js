66e9f34161689fbb13852bb6961bfaeee4b2e2b3
David Leach <davidleach724@gmail.com>
2021-09-23 13:37:56 -0600

    Nav height and color

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import LogOn from '../LogOn/LogOn';
+import NavBar from '../NavBar/NavBar';
 import './App.css';
 
 const App = () => {
   return (
     <main className="App">
-      <h1>BAND GLAMP MOTHA FUCKA</h1>
+      <NavBar />
       <Switch>
         <Route exact path='/' component={ LogOn } />
       </Switch>
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import '../NavBar/NavBar.css'
+
+const NavBar = () => {
+  return (
+    <nav className="nav-container">
+      <h1>BAND GLAMP MOTHA FUCKAA</h1>
+    </nav>
+  )
+}
+
+export default NavBar
