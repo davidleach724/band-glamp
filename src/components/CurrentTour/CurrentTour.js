5eaf329b12708081352c681fa2f32dd78805afba
David Leach <davidleach724@gmail.com>
2021-09-23 16:03:48 -0600

    Add route to currentTour

---
+++ src/components/CurrentTour/CurrentTour.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './CurrentTour.css'
+
+const CurrentTour = () => {
+  return (
+    <h2>This is the current Tour Section yall</h2>
+  )
+}
+
+export default CurrentTour
