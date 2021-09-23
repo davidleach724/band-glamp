0aaff0c0025b43d12a25ee159b0b2da08ed5dfe2
David Leach <davidleach724@gmail.com>
2021-09-23 09:53:58 -0600

    Create LogOn component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import LogOn from '../LogOn/LogOn';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
+    <main className="App">
       <h1>BAND GLAMP MOTHA FUCKA</h1>
-    </div>
+      <Switch>
+        <Route exact path='/' component={ LogOn } />
+      </Switch>
+    </main>
   );
 }
 
---
+++ src/components/LogOn/LogOn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../LogOn/LogOn.css'
+
+const LogOn = () => {
+
+  return (
+    <h2>This is the log on bitchessss</h2>
+  )
+}
+
+export default LogOn
