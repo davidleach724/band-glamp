d835e5bd88c853271839c41727d65c3829c8dc83
David Leach <davidleach724@gmail.com>
2021-09-24 08:26:51 -0600

    Add link to indiv shows and remove def styling

--- src/components/SmallDates/SmallDates.js
+++ src/components/SmallDates/SmallDates.js
@@ -1,13 +1,16 @@
+import { Link } from 'react-router-dom'
 import './SmallDates.css'
 
 const SmallDates = ({ show }) => {
   return (
+    <Link to={`/currentTour/${show.id}`} style={{textDecoration: 'none'}}>
     <section>
       <p>{show.date}</p>
       <p>{show.venue}</p>
       <p>{show.city}</p>
       <p>{show.state}</p>
     </section>
+    </Link>
   )
 }
 
