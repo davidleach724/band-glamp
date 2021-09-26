fc4015af1dad6b27a418d3db415f6e7e838dbd45
David Leach <davidleach724@gmail.com>
2021-09-26 10:45:10 -0600

    Create Error component

---
+++ src/components/Error/Error.js
@@ -0,0 +1,10 @@
+import errorImg from '../../images/pagenotfound.png'
+import './Error.css'
+
+const Error = () => {
+  return (
+    <img src={errorImg} alt="page not found" />
+  )
+}
+
+export default Error
