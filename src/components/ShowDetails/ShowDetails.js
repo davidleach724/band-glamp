f78d67c89b4c84643cacd2cd9669954d3010916d
David Leach <davidleach724@gmail.com>
2021-09-27 15:07:14 -0600

    Add heres the camp banner

--- src/components/ShowDetails/ShowDetails.js
+++ src/components/ShowDetails/ShowDetails.js
@@ -1,5 +1,6 @@
 import Error from '../Error/Error'
 import heresGig from '../../images/heres-the-gig.png'
+import heresCamp from '../../images/heres-the-camp.png'
 import currentTour from '../../images/current-tour.png'
 import './ShowDetails.css'
 import { Link } from 'react-router-dom'
@@ -19,6 +20,7 @@ const ShowDetails = ({ selectedShow }) => {
         <p>Venue: {selectedShow.venue}</p>
         <p>{selectedShow.city}, {selectedShow.state}</p>
         <p>Details: {selectedShow.notes}</p>
+        <img src={heresCamp} alt="heres the camp banner" className="here-camp"/>
         <img src={selectedShow.camp_img} alt="chosen camp" className="camp-img"/>
         <p>Campsite: {selectedShow.camp_name}</p>
         <p>Address: {selectedShow.camp_address}</p>
