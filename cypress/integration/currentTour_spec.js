53ead68e6e15c928b42468cd9ff4be3bd0ba66da
David Leach <davidleach724@gmail.com>
2021-09-27 14:36:02 -0600

    Add CurrentTour tests

--- cypress/integration/currentTour_spec.js
+++ cypress/integration/currentTour_spec.js
@@ -18,3 +18,16 @@ it('should have 2 buttons to go to current tour or to add a date', () => {
 it('should display dates upon page load', () => {
   cy.get('[href="/currentTour/01"] > section')
 })
+
+it('should redirect when a date is selected', () => {
+  cy.get('section')
+  .contains('Levitt Pavillion')
+  .click()
+  .url().should('include', '/currentTour/01')
+})
+
+it('should render a 404 page when url is incorrect', () => {
+  cy.visit('http://localhost:3000/westernNight')
+    .get('.page-not-found') 
+    .get('.logo')
+})
